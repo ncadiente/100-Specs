58dd4d949cc23011f43fae004a2af86c7fb4cb43
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-15 18:07:52 -1000

    adds Princess Leia Class

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -638,8 +638,33 @@ SolarSystem.prototype.removePlanet = function(planet) {
  *   marries
  *
  */
+function PrincessLeia(name, money, age, gender) {
+  Person.call(this, name, money, age, gender);
+  this.isInTrouble = null;
+}
 
+PrincessLeia.prototype = Object.create(Person.prototype);
 
+PrincessLeia.prototype.shootsGun = function() {
+  this.isInTrouble = false;
+  return "Leia shoots her gun wildly";
+};
+
+PrincessLeia.prototype.getsInTrouble = function() {
+  this.isInTrouble = true;
+  return "Help me Obi-wan Kenobi, you're my only hope";
+};
+
+PrincessLeia.prototype.marries = function(person) {
+  if (person === "Luke Skywalker") {
+    return "Gross!";
+  }
+  if (person === "Han Solo") {
+    return true;
+  } else {
+    return false;
+  }
+};
 /* Step 34
  *
  * Define a class named "Stapler" with properties "color"
